8c6edea08f8f24fc7c9137697f5f7a992dc3ad1d
Mathéo Berthomé-Laurent <berthomath@hotmail.com>
2025-05-22 18:03:34 +0200

    Fetch Added

--- Scripts/data.js
+++ Scripts/data.js
@@ -1,19 +1,21 @@
-fetch("../Data/mario-games.json")
-    // Quand le fichier est bien trouvé, on convertit la réponse en JSON
-    .then(response => response.json())
+function LoadGames(id,JsonFile){
+  fetch(JsonFile)
+      .then(response => response.json())
+      .then(gameImages => {
+        const gameList = document.getElementById(id);
 
-    .then(gameImages => {
-        // On sélectionne l'endroit où insérer les images
-        const gameList = document.querySelector(".card-games");
-
-        // Pour chaque URL d’image récupérée...
         gameImages.forEach(src => {
             const img = document.createElement("img");
             img.src = src;
             img.alt = "Image du jeu Mario";
-
-            // On ajoute l’image à la liste des jeux
             gameList.appendChild(img);
         });
-    })
+      })
     .catch(error => console.error("Erreur lors du chargement des jeux :", error));
+}
+
+LoadGames("mario-games", "../Data/mario-games.json");
+LoadGames("luigi-games", "../Data/luigi-games.json");
+LoadGames("peach-games", "../Data/peach-games.json");
+LoadGames("toad-games", "../Data/toad-games.json");
+LoadGames("bowser-games", "../Data/bowser-games.json");
