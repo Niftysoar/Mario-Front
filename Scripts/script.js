ceddcc93c9edbe6e32b350997b0f4929ff754cb5
Mathéo Berthomé-Laurent <berthomath@hotmail.com>
2025-06-03 11:24:21 +0200

    Character card can be closed and opened

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,24 +1,4 @@
-class CharacterCard {
-    constructor(id, backgroundPattern) {
-        this.card = document.getElementById(id);
-        this.backgroundPattern = backgroundPattern;
-    }
-
-    applyBackground() {
-        if (this.card) {
-          this.card.style.backgroundImage = `url(${this.backgroundPattern})`;
-        }
-    }
+function toggleCard(cardId) {
+  const card = document.getElementById(cardId);
+  card.classList.toggle("closed");
 }
-
-// Liste des personnages avec leurs motifs de fond
-const characters = [
-      new CharacterCard("card-mario", "../Images/MarioHub_Characters_CharBtn_bg_Mario.png"),
-      new CharacterCard("card-luigi", "../Images/MarioHub_Characters_CharBtn_bg_Luigi.png"),
-      new CharacterCard("card-peach", "../Images/MarioHub_Characters_CharBtn_bg_Peach.png"),
-      new CharacterCard("card-toad", "../Images/MarioHub_Characters_CharBtn_bg_Toad.png"),
-      new CharacterCard("card-bowser", "../Images/MarioHub_Characters_CharBtn_bg_Bowser.png")
-];
-
-// Application des backgrounds
-characters.forEach(character => character.applyBackground());
