15e63c3176186a257f9f8be83a01624723740733
Mathéo Berthomé-Laurent <berthomath@hotmail.com>
2025-07-05 15:30:31 +0200

    Added Mario Font / Assets folder added / Better Responsive

--- Scripts/CharacterCard.js
+++ Scripts/CharacterCard.js
@@ -13,11 +13,11 @@ class CharacterCard {
 
 // Liste des personnages avec leurs motifs de fond
 const characters = [
-      new CharacterCard("card-mario", "../Images/MarioHub_Characters_CharBtn_bg_Mario.png"),
-      new CharacterCard("card-luigi", "../Images/MarioHub_Characters_CharBtn_bg_Luigi.png"),
-      new CharacterCard("card-peach", "../Images/MarioHub_Characters_CharBtn_bg_Peach.png"),
-      new CharacterCard("card-toad", "../Images/MarioHub_Characters_CharBtn_bg_Toad.png"),
-      new CharacterCard("card-bowser", "../Images/MarioHub_Characters_CharBtn_bg_Bowser.png")
+      new CharacterCard("card-mario", "../Assets/Images/MarioHub_Characters_CharBtn_bg_Mario.png"),
+      new CharacterCard("card-luigi", "../Assets/Images/MarioHub_Characters_CharBtn_bg_Luigi.png"),
+      new CharacterCard("card-peach", "../Assets/Images/MarioHub_Characters_CharBtn_bg_Peach.png"),
+      new CharacterCard("card-toad", "../Assets/Images/MarioHub_Characters_CharBtn_bg_Toad.png"),
+      new CharacterCard("card-bowser", "../Assets/Images/MarioHub_Characters_CharBtn_bg_Bowser.png")
 ];
 
 // Application des backgrounds
